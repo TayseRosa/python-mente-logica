eccc6ea5662546049809c0fea36c6e41ba46cb30
Tayse Rosa <tayse_rosa@yahoo.com.br>
2024-11-27 11:49:56 -0300

    Python - Desafios Mente Lógica

---
+++ dia2.py
@@ -0,0 +1,36 @@
+#variaveis
+idade = 36
+nome = "Tayse"
+altura = 1.56
+
+#Tipos de dados
+#int, float, strings
+cidade = 'São Paulo'
+print(nome)
+logado = True
+nome = 2
+print(nome)
+
+##Verificar tipo de dado
+print(type(nome))
+print(type(altura))
+print(type(cidade))
+print(type(logado))
+
+#Operadores
+print(5 + 1.2)
+print(10 * 2)
+print(5 / 10)
+print(50 - 25)
+
+#Concatenacao com +
+print("Olá " + "tudo bem?")
+
+#comparacao
+maior = 15 > 2
+print(maior)
+print(5 == 5)
+print('Tayse' == 'tayse')
+print( 15!=2)
+num=12
+print('Numero: ',num)
---
+++ dia3.py
@@ -0,0 +1,25 @@
+#Operadores aritmeticos
+a=5
+b=2
+
+print(a + b)
+print(a * b)
+print(a / b)
+
+#Exponenciação **
+#Divisão inteira //
+print(a // b)
+print(a / b)
+print(a % b)
+
+#Operacdores de comparação
+print(10 > 5) 
+print(10 < 5) 
+print(10 != 5) 
+print(10 >= 5) 
+print(10 >= 5) 
+
+idade = 2
+tem_carteira = True
+pode_dirigir = (idade > 18) and tem_carteira
+print(pode_dirigir)
