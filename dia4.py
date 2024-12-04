9a81cb19c3d6dfd68a84d6fdfc4734631f34326a
Tayse Rosa <tayse_rosa@yahoo.com.br>
2024-12-03 21:47:56 -0300

    Dia 5

--- dia4.py
+++ dia4.py
@@ -1,1 +1,9 @@
 #Condicionais If, Else
+idade = 19
+estuda_rede_publica = False
+
+if idade >= 18 or estuda_rede_publica :
+        print("Tem direito a meia entrada")
+else:
+    print("menor de 18")
+
---
+++ dia5.py
@@ -0,0 +1,13 @@
+frutas = ["maçã", "banana", "laranja"]
+
+for frutas in frutas:
+        print("Fruta: ", frutas)
+
+
+for i in range(5):
+        print("Num: ", i)
+
+contador=1
+while contador < 5:
+        print("Num while: ", contador)
+        contador +=1
